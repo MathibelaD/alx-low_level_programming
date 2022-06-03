45edd19fe913d9396b4aaf1415408e64ab4e6f28
Dineo Mathibela <dineomathibela161@gmail.com>
2022-06-02 20:28:05 -0700

    Task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Main 
+ *
+ * return 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)",sizeof(char);
+	printf("Size of a char: %d byte(s)",sizeof(int);
+	printf("Size of a char: %d byte(s)",sizeof(long int);
+	printf("Size of a char: %d byte(s)",sizeof(long long int;
+	printf("Size of a char: %d byte(s)",sizeof(float);
+
+	return (0);	
+
+}	
