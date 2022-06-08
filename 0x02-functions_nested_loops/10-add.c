886b9c596d302846a84784d840e2635b16e2c358
Dineo Mathibela <dineomathibela161@gmail.com>
2022-06-08 03:27:03 -0700

    Task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function
+ *
+ * @num1: first variable
+ * @num2: second variable
+ * Return: result
+ */
+int add(int num1, int num2)
+{
+	int result = num1 + num2;
+
+	return (result);
+}
