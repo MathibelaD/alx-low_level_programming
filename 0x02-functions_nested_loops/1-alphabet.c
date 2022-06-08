25aaf683106c8e5bbb9dbc5bf733f0a799561480
Dineo Mathibela <dineomathibela161@gmail.com>
2022-06-07 19:54:11 -0700

    TAsk1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+}
