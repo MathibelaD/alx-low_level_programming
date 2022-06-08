ecfa6205fb715042caf703f7b26b777c4ec49be9
Dineo Mathibela <dineomathibela161@gmail.com>
2022-06-08 15:45:36 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,16 +9,17 @@ int main(void)
 {
 	int max = 50, num1 = 1, num2 = 2, num3 = 0, i = 0;
 
-	printf("%d, %d, ", num1, num2);
+	printf("%d", num1);
 	while (i < max)
 	{
 		num3 = num1 + num2;
+		printf(", %d", num3);
 		num1 = num2;
 		num2 = num3;
 
-		printf(", %d", num3);
 		i++;
 	}
+	putchar(10);
 
 	return (0);
 }
