4932ae17208996d7ae9a2849a8390ce3c9e2e4f9
Dineo Mathibela <dineomathibela161@gmail.com>
2022-06-08 15:04:34 -0700

    Task10

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int hour = 0, hour1, hour2, minute, minute1, minute2;
+
+	while (hour < 23)
+	{
+		hour1 = hour / 10;
+		hour2 = hour % 10;
+		minute = 0;
+
+		while (min < 60)
+		{
+			minute1 = minute / 10;
+			minute2 = minute % 10;
+
+			_putchar(hour1 + '0');
+			_putchar(hour2 + '0');
+			_putchar(':');
+			_putchar(minute1 + '0');
+			_putchar(minute2 + '0');
+
+			minute++;
+		}
+		hour++;
+	}
+}
