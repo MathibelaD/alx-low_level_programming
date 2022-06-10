3942fdacdb11ef20b898d3b02e1866899cf319ef
Dineo Mathibela <dineomathibela161@gmail.com>
2022-06-10 12:17:04 -0700

    Task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function
+ * @a: character variable
+ * @b: character variable
+ * Return: multiple of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
