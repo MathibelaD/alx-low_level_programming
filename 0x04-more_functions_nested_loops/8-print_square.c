2ec95ca524beffb7469423aca58a235d95e5fdba
Dineo Mathibela <dineomathibela161@gmail.com>
2022-06-10 12:59:59 -0700

    Task8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - function
+ * @size: variable
+ * Return: #
+ */
+
+void print_square(int size)
+{
+	int i = 1, x = 0;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i <= size)
+		{
+			for (x = 1; x <= size; x++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+	_putchar('\n');
+}
