25f54b21d10dfbba39ce4e826a1bc00c3ec68177
Dineo Mathibela <dineomathibela161@gmail.com>
2022-06-10 11:48:15 -0700

    Task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - function
+ * @c: character variable
+ * Return: 1 if c is uppercase or 0 if not
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
+}
