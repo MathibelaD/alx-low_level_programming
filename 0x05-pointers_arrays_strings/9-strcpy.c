2dbe54a897f7b5e96f583040c413ac42ccd50630
Dineo Mathibela <dineomathibela161@gmail.com>
2022-06-14 19:24:51 -0700

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * *_strcpy - function
+ * @dest: pointer varuable
+ * @src: pointer variable
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return (dest);
+}
