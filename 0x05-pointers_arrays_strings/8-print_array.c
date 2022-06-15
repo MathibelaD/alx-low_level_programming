7c509ca83af44b8144405ef082c3af455f00097c
Dineo Mathibela <dineomathibela161@gmail.com>
2022-06-14 19:07:33 -0700

    Print araay

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_array - function
@@ -11,7 +12,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", a[i]);
+		printf("%d", a[i]);
+		if (i == (n - 1))
+			break;
+		_putchar(',');
+		_putchar(' ');
 	}
 	putchar('\n');
 }
