315514b91493703682407970b5cec06b0d8a7967
Dineo Mathibela <dineomathibela161@gmail.com>
2022-06-16 17:02:16 -0700

    string compare Task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - function
+ * @s1: parameter variable
+ * @s2: parameter variable
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
