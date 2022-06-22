01fa4ab57b1a03b2530ae6569f38cb156c499c5e
Dineo Mathibela <dineomathibela161@gmail.com>
2022-06-22 16:19:07 -0700

    TAsk2- recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * _strlen_recursion - function
+ * @s: parameter variable
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
