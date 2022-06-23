637b29a3f000b39e62106952920c870c35a1aad9
Dineo Mathibela <dineomathibela161@gmail.com>
2022-06-22 17:32:06 -0700

    TAsk5 - recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function
+ * @n: parameter
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n / _sqrt_recursion(n - 1));
+}
