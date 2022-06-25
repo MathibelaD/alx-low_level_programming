fdde64518bff1bebce04d84e14bd4531b5734c22
Dineo Mathibela <dineomathibela161@gmail.com>
2022-06-24 20:12:20 -0700

    Tas4 - adding

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "ctype.h"
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: parameter
+ * @argv: parameter
+ * Return: 1 if erro else 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc < 1)
+		puts("0");
+
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(atoi(argv[i])))
+		{
+			puts("Error");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
