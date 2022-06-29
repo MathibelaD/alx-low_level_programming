5e2e670668514df8279020a0767b5b4971a1604e
Dineo Mathibela <dineomathibela161@gmail.com>
2022-06-29 16:29:20 -0700

    Task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,1 +1,17 @@
 #include "main.h"
+#include <stdlib.h>
+#include "string.h"
+
+/**
+ * _strdup - function
+ * @str: parameter
+ * Return: pointer or NULL
+ */
+char *_strdup(char *str)
+{
+	char *s = malloc(str * sizeof(char));
+
+	if (str == NULL)
+		return (NULL);
+	return (strdup(str));
+}
