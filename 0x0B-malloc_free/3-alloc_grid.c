7424a7f222482ea67e8babeea3f80095134c0997
Dineo Mathibela <dineomathibela161@gmail.com>
2022-06-29 17:12:24 -0700

    TAsk3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,1 +1,22 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function
+ * @width: parameter
+ * @height: parameter
+ * Return: int
+ */
+int **alloc_grid(int width, int height)
+{
+	width = 0;
+	height = 0;
+
+	int *p = malloc(INT_MAX);
+
+	if (p == NULL)
+		return (NULL);
+	if (width <= 0 || height <= 0)
+		return (NULL);
+}
